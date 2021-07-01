4d6941f91c9c15d6c532f2281affebad02f33fb8
devemg <garciam.emm@gmail.com>
2021-07-01 11:51:59 -0600

    add first components

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+
+    render() {
+        return <div>
+            <div className="footer-left">
+
+            </div>
+            <div className="footer-right">
+
+            </div>
+        </div>
+    }
+}
+
+export default Footer;

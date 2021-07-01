ae018a81e4ef7acaa1e0a8402be58762afc5c6b9
devemg <garciam.emm@gmail.com>
2021-07-01 12:17:46 -0600

    first stilying

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Message.scss';
 
 /**
  * 2nd Section
@@ -10,7 +11,7 @@ class Message extends React.Component {
         return <section id="message">
         <div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus odio nullam maecenas diam neque egestas. Pellentesque nisl faucibus id praesent pretium lorem nulla feugiat tortor. Id augue ullamcorper integer vulputate vitae elementum bibendum. Fermentum, at at lectus lacus, commodo. Curabitur orci nisi, diam, aenean dignissim interdum. Quis justo, neque condimentum tempus dolor proin sed. Neque, duis in lacinia sit orci. Duis sem ipsum ullamcorper urna porttitor nunc, quis libero, lectus. Aliquam nibh vitae, morbi quisque egestas. Pellentesque maecenas in id egestas facilisi at. Adipiscing malesuada bibendum porta nec. Nunc rhoncus, ante a lobortis sit. Amet, ut proin elit lectus dui.</p>
-        <div class="signature">
+        <div className="signature">
             <h3>- Emely</h3>
         </div>
         </div>

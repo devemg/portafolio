c371898ac55080093a8599ec87b21c485995def5
Emely Garcia <garciam.emm@gmail.com>
2021-07-14 19:49:23 -0600

    fix name

--- src/Components/Start/Start.js
+++ src/Components/Start/Start.js
@@ -12,9 +12,9 @@ class Start extends React.Component {
     render() {
         return <section id="home">
         <div className="container-name" data-aos="fade-up" data-aos-duration={this.props.animationDuration}>
-            <h1>Hello!</h1>
-            <h1>I’m Emely Garcia</h1>
-            <h1>Web Developer</h1>
+            <h1>¡Hola!</h1>
+            <h1>Soy Emely Garcia</h1>
+            <h1>Desarrolladora de Software</h1>
             <a href="#message" className="image-arrow">
                 <img src={arrowImage} alt="arrow"/>
             </a>

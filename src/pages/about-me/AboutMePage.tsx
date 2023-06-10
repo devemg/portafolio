19741e359ab03d174682594826f5d3b45743bd2a
Emely Garcia <garciam.emm@gmail.com>
2023-06-09 21:57:04 -0600

    Init new version

---
+++ src/pages/about-me/AboutMePage.tsx
@@ -0,0 +1,11 @@
+import './AboutMePage.scss';
+
+export const AboutMePage = () => {
+  return (
+    <div className='about'>
+        <h1>Emely Garcia</h1>
+        <h2>Angular Developer</h2>
+        <p>I am a dedicated and enthusiastic software developer. I have been developing software for more than four years for the academic area and since one year ago in the professional area. I am curious and self-taught. Constantly learning and preparing myself to face any challenge and looking to do my job as efficiently as possible. most efficient way possible.</p>
+    </div>
+  )
+}

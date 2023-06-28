eadd50082825b542c5e61f24f3e87c78525c0a12
Emely Garcia <garciam.emm@gmail.com>
2023-06-27 20:57:29 -0600

    Manage form

--- src/components/form/FormComponent.tsx
+++ src/components/form/FormComponent.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "../../hooks/useForm";
 import "./FormComponent.scss";
 
 export interface FormField {
@@ -5,28 +6,39 @@ export interface FormField {
   name: string;
   type: string;
   placeholder: string;
+  initValue: unknown;
+  validator: [(value: any) => boolean, string]; // callback validation, errorMessage
 }
 
+
 export const FormComponent = ({ fields, buttonText }: any) => {
+  const { onInputChange, formState, isFormValid, formValidation } = useForm(fields);
+
+  const onsubmit = (event: any) => {
+    event.preventDefault();
+    console.log(formState);
+  }
+  
   return (
     <form className="form">
       {fields.map((field: FormField) => (
         <div key={field.name} className="form-field">
           <label htmlFor={field.name}>{field.label}</label>
           {field.type === "textarea" ? (
-            <textarea name={field.name} rows={3}
+            <textarea name={field.name} rows={3} onChange={onInputChange}
             placeholder={field.placeholder}></textarea>
           ) : (
             <input
               type={field.type}
               name={field.name}
               placeholder={field.placeholder}
+              onChange={onInputChange}
             />
           )}
-          <span className="form-field-error"></span>
+          <span className="form-field-error">{formValidation[field.name]}</span>
         </div>
       ))}
-      <button className="primary-button" type="submit">{buttonText}</button>
+      <button disabled={!isFormValid} className={["primary-button", !isFormValid ? 'disabled' : ''].join(' ')} onClick={onsubmit} type="submit">{buttonText}</button>
     </form>
   );
 };
